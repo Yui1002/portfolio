1a78a29116307cdc7e02925dab8d314ef70de56b
yui_dayal <murayamayuui@Yuis-MacBook-Pro.local>
2025-04-03 19:51:29 +0900

    created sample project data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Project from "./Project";
 
 const Projects = () => {
+    
     return (
         <div>
             <p className="title">Projects</p>
---
+++ src/projectData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+    {
+        name: "Lesson Manager",
+        description: "This project is about .....",
+        image: "",
+        githubLink: "https://github.com/Yui1002/LessonManager"
+    },
+    {
+        name: "Timebox",
+        description: "This project is a timekeeping mobile app designed for employees. It enables users to clock in and out seamlessly, ensuring accurate tracking of work hours. Both employers and employees can access time records, while employers also have the ability to send hiring requests directly through the app.",
+        image: "",
+        githubLink: "https://github.com/Yui1002/timebox"
+    }
+]
+
+export default projectsData;
