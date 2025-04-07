57c95f93a01f5ef052aebab1de399ace91d4d211
yui_dayal <murayamayuui@Yuis-MacBook-Pro.local>
2025-04-07 11:38:18 +0900

    save current work

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,17 @@
 import Project from "./Project";
 import projectData from "../projectData";
+import '../styles/Projects.css'
 
 const Projects = () => {
     
     return (
         <div className="projects-container">
             <p className="title">Projects</p>
-            {projectData.map((project, index) => (
-                <Project project={project} key={index} />
-            ))}
+            <div className="projects-sub-container">
+                {projectData.map((project, index) => (
+                    <Project project={project} key={index} />
+                ))}
+            </div>
         </div>
     )
 }
